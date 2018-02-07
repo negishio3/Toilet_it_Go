35435e4d4d873412f701429040dbcc73c37cd8fa
HaruRabbi <nkc.harutaka.chikazawa@gmail.com>
2018-02-07 13:50:10 +0900

    シーン遷移仮実装。細かい所は全く手を触れてない

---
+++ Assets/Chika/Sprict/Move.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float speed; //歩くスピード
+    private Rigidbody2D rigidbody2D;
+    public GameObject mainCamera;
+
+    // Use this for initialization
+    void Start () {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //左キー: -1、右キー: 1
+        float x = Input.GetAxisRaw("Horizontal");
+        //左か右を入力したら
+        if (x != 0)
+        {
+            //入力方向へ移動
+            rigidbody2D.velocity = new Vector2(x * speed, rigidbody2D.velocity.y);
+            //localScale.xを-1にすると画像が反転する、これで歩く方向に体が向く
+            Vector2 tempScl = transform.localScale;
+            transform.localScale = tempScl;
+        }
+        else
+        {
+            //横移動の速度を0にしてピタッと止まるようにする
+            rigidbody2D.velocity = new Vector2(0, rigidbody2D.velocity.y);
+        }
+    }
+}
