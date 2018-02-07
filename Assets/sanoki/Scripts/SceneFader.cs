c0ba785fa35cb94719f8a7a54922349fc3f6a0c0
NaRu <nkc.hirokazu.sanoki@gmail.com>
2018-02-07 19:48:09 +0900

    2/7 自分の作業がひと段落

---
+++ Assets/sanoki/Scripts/SceneFader.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneFader : MonoBehaviour
+{
+    //////////////////////////////////////////////////////////
+    // このscriptはシーン上のCanvasにアタッチしてください。/// 
+    //////////////////////////////////////////////////////////
+
+    float red, green, blue; //RGBを操作するための変数
+    public static string next_Scene;
+    public static bool isFade = false;//フェード中かどうか
+
+    int fade;//FadeIn：１ FadeOut：０
+
+    Color fadeColor;
+
+    float fadeSpeed = 1.5f;
+
+    void Start()
+    {
+       fade = 0;
+        StartCoroutine(SceneFade(fadeSpeed, false));
+    }
+
+    /// <summary>
+    /// 引数のシーンにフェードして遷移する
+    /// </summary>
+    /// <param name="nextSceneName">遷移先のシーン名</param>
+    public void StageSelect(string nextSceneName)
+    {
+        if (!isFade)
+        {
+            isFade = true;
+            next_Scene = nextSceneName;
+            fade = 1;
+            StartCoroutine(SceneFade(fadeSpeed,true));
+        }
+    }
+
+    /// <summary>
+    /// フェードだけする
+    /// </summary>
+    public void OnFade(float time)
+    {
+        if (!isFade)
+        {
+            isFade = true;
+            fade = 1;
+            StartCoroutine(SceneFade(time, false));
+        }
+    }
+
+    //フェード用のGUI
+    void OnGUI()
+    {   
+        GUI.color = fadeColor;
+        GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),Texture2D.whiteTexture);
+    }
+
+    /// <summary>
+    /// フェード用コルーチン
+    /// </summary>
+    /// <param name="second">何秒かけてフェードさせるのか</param>
+    /// <param name="sceneChange">シーン遷移させるのかどうか(true:させる　false:させない)</param>
+    /// <returns></returns>
+    public IEnumerator SceneFade(float second,bool sceneChange)
+    {
+        
+
+        float t = 0.0f;
+        switch (fade)
+        {
+            case 1:
+                Debug.Log("フェードイン");
+                while (t < 1.0f)
+                {
+                    t += Time.deltaTime / second;
+                    fadeColor.a = Mathf.Lerp(0.0f, 1.0f, t);
+                    yield return null;
+                }
+                if (sceneChange) { SceneManager.LoadScene(next_Scene); }
+                else
+                {
+                    fade = 0;
+                    StartCoroutine(SceneFade(fadeSpeed, false));
+                }
+                break;
+            case 0:
+                while (t < 1.0f)
+                {
+                    t += Time.deltaTime / second;
+                    fadeColor.a = Mathf.Lerp(1.0f, 0.0f, t);
+                    yield return null;
+                }
+                isFade = false;
+                break;
+        }
+    }
+}
---
+++ Assets/sanoki/Scripts/TrainMove.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrainMove : MonoBehaviour {
+
+    public GameObject UNKOman;
+    float UNKOman_Speed = 4;
+    public GameObject[] BackImagePrefab;//ステージのプレハブ
+    Vector3 StartPos = new Vector2(60,0);//生成位置
+    public float scrollSpeed = 1;//スクロールの速度
+    GameObject[] image = new GameObject[2];//生成したステージを管理する
+    bool moveFlg= true;
+    public int stageCount = 0;
+    public int maxScrollNum;
+
+	void Start () {
+
+        TrainImageInstans(2);//初期背景の生成
+        TrainImageInstans(1);
+        //image[1] = Instantiate(
+        //    BackImagePrefab[1],
+        //    StartPos,
+        //    Quaternion.identity);
+    }
+	
+	void Update () {
+
+        if (image[0].transform.position.x <= -60)
+        {
+            if (stageCount == maxScrollNum)
+            {
+                
+            }
+            TrainImageInstans(0);
+        }
+        if (image[1].transform.position.x <= -60)
+        {
+            TrainImageInstans(1);
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            scrollSpeed = 3;
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            scrollSpeed = 10;
+        }
+        if (stageCount==maxScrollNum)
+        {
+            scrollSpeed = 3;
+            //moveFlg = false;
+        }
+        StartCoroutine(TrainMoving());
+        UNKOman.transform.position += new Vector3(Time.deltaTime * Input.GetAxisRaw("Horizontal") * UNKOman_Speed,0);
+	}
+    /// <summary>
+    /// ステージの生成
+    /// </summary>
+    void TrainImageInstans(int ImageNum)
+    {
+        switch (ImageNum)
+        {
+            case 0:
+                Destroy(image[0]);
+                image[0] = Instantiate(
+                    BackImagePrefab[0],
+                    StartPos,
+                    Quaternion.identity);
+                break;
+            case 1:
+                Destroy(image[1]);
+                image[1] = Instantiate(
+                    BackImagePrefab[1],
+                    StartPos,
+                    Quaternion.identity);
+                break;
+            case 2:
+                image[0] = Instantiate(
+                    BackImagePrefab[0],
+                    Vector2.zero,
+                    Quaternion.identity);
+                break;
+        }
+        stageCount++;
+        //if(!image[1]==null)
+            
+    }
+    /// <summary>
+    /// 背景を動かす
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator TrainMoving()
+    {
+        if (!moveFlg)//moveFlgがfalseなら中断
+            yield break;
+        image[0].transform.position -= new Vector3(Time.deltaTime * scrollSpeed, image[0].transform.position.y, 0);
+        image[1].transform.position -= new Vector3(Time.deltaTime * scrollSpeed, image[1].transform.position.y, 0);
+        yield return null;
+    }
+}
