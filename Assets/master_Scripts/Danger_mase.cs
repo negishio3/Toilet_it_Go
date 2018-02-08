fbd648e6c814b01f2e946417f2262ad1b8881ba9
yuya0915 <nkc.yuya.mase.2016@gmail.com>
2018-02-08 13:47:24 +0900

    タイム制作

--- Assets/master_Scripts/Danger_mase.cs
+++ Assets/master_Scripts/Danger_mase.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Danger_mase : MonoBehaviour
 {
+
     public GameObject Player;
-	// Use this for initialization
-	void Start ()
+
+
+    // Use this for initialization
+    void Start ()
     {
 		
 	}
@@ -14,15 +17,9 @@ public class Danger_mase : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Player.SetActive(true);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            Player.SetActive(false);
-        }
+
     }
+
 
     public void OnClick()
     {
