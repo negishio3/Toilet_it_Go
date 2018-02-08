2dde469dbe420378e68b14f514ee811026c7cb14
yuya0915 <nkc.yuya.mase.2016@gmail.com>
2018-02-08 09:38:30 +0900

    コミット

--- Assets/mase/Script/Danger.cs
+++ Assets/mase/Script/Danger.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Danger : MonoBehaviour
 {
-    public GameObject Player;
+
 	// Use this for initialization
 	void Start ()
     {
@@ -14,14 +14,7 @@ public class Danger : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Player.SetActive(true);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            Player.SetActive(false);
-        }
+
     }
 
     public void OnClick()
