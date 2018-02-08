a786a09feb1cc43ff3cd415ad8f1b0d0f52fabd5
NaRu <nkc.hirokazu.sanoki@gmail.com>
2018-02-08 15:01:39 +0900

    今日の最新(2/8)

---
+++ Assets/master_Scripts/Goalprogram_sanoki.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goalprogram_sanoki : MonoBehaviour {
+
+    SceneFader_sanoki sf;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Goal")
+        {
+            Debug.Log("当たった");
+            sf.StageSelect("sanoki_Title");
+        }
+    }
+}
--- Assets/master_Scripts/TrainMove_sanoki.cs
+++ Assets/master_Scripts/TrainMove_sanoki.cs
@@ -5,39 +5,51 @@ using UnityEngine.UI;
 
 public class TrainMove_sanoki : MonoBehaviour {
 
-    public GameObject UNKOman;
-    float UNKOman_Speed = 4;
+    public GameObject UNKOman;//キャラクター
+    float UNKOman_Speed = 4;  //キャラクターの移動速度
     public GameObject[] BackImagePrefab;//ステージのプレハブ
-    Vector3 StartPos = new Vector2(60,0);//生成位置
+    Vector3 InstansPos = new Vector2(60,0);//生成位置
     public float scrollSpeed = 1;//スクロールの速度
     GameObject[] image = new GameObject[2];//生成したステージを管理する
-    bool moveFlg= true;
-    public int stageCount = 0;
-    public int maxScrollNum;
+    bool moveFlg;//背景を止めるか否か
+    public int stageCount = 0;//生成された背景のカウンター
+    public int maxScrollNum;//生成する背景の最大値
+    public float maxScrollTime;//生成し続ける時間
+
+    public float timer;
+    bool isCountDown;
 
 	void Start () {
-
-        TrainImageInstans(2);//初期背景の生成
-        TrainImageInstans(1);
-        //image[1] = Instantiate(
-        //    BackImagePrefab[1],
-        //    StartPos,
-        //    Quaternion.identity);
+        isCountDown = false;
+        TrainImageInstans(0);//初期背景の生成
     }
 	
 	void Update () {
-
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameStart();
+        }
         if (image[0].transform.position.x <= -60)
         {
-            if (stageCount == maxScrollNum)
+            if (stageCount >= maxScrollNum)
             {
-                
+                TrainImageInstans(2);
             }
-            TrainImageInstans(0);
+            else
+            {
+                TrainImageInstans(1);
+            }
         }
         if (image[1].transform.position.x <= -60)
         {
-            TrainImageInstans(1);
+            if (stageCount >= maxScrollNum)
+            {
+                TrainImageInstans(2);
+            }
+            else
+            {
+                TrainImageInstans(1);
+            }
         }
         if (Input.GetMouseButtonDown(0))
         {
@@ -47,7 +59,8 @@ public class TrainMove_sanoki : MonoBehaviour {
         {
             scrollSpeed = 10;
         }
-        if (stageCount==maxScrollNum)
+        //if (stageCount>maxScrollNum)
+        if(timer > maxScrollTime)
         {
             scrollSpeed = 3;
             //moveFlg = false;
@@ -55,37 +68,78 @@ public class TrainMove_sanoki : MonoBehaviour {
         StartCoroutine(TrainMoving());
         UNKOman.transform.position += new Vector3(Time.deltaTime * Input.GetAxisRaw("Horizontal") * UNKOman_Speed,0);
 	}
+
+    /// <summary>
+    /// ゲームを開始する簡易メソッド
+    /// </summary>
+    public void GameStart()
+    {
+        moveFlg = true;
+        StartCoroutine(Timer(maxScrollTime));
+    }
+
     /// <summary>
     /// ステージの生成
     /// </summary>
+    /// <param name="ImageNum">番号によって生成方法を変える(整数)</param>
     void TrainImageInstans(int ImageNum)
     {
         switch (ImageNum)
         {
-            case 0:
-                Destroy(image[0]);
-                image[0] = Instantiate(
-                    BackImagePrefab[0],
-                    StartPos,
-                    Quaternion.identity);
-                break;
-            case 1:
-                Destroy(image[1]);
-                image[1] = Instantiate(
-                    BackImagePrefab[1],
-                    StartPos,
-                    Quaternion.identity);
-                break;
-            case 2:
+            case 0: //初期生成
                 image[0] = Instantiate(
                     BackImagePrefab[0],
                     Vector2.zero,
                     Quaternion.identity);
+                stageCount++;
+                image[1] = Instantiate(
+                        BackImagePrefab[1],
+                        InstansPos,
+                        Quaternion.identity);
+                break;
+            case 1: //ループ生成
+                if (image[0].transform.position.x <= -60)
+                {
+                    Destroy(image[0]);
+                    image[0] = Instantiate(
+                        BackImagePrefab[0],
+                        InstansPos,
+                        Quaternion.identity);
+                }
+                if (image[1].transform.position.x <= -60)
+                {
+                    Destroy(image[1]);
+                    image[1] = Instantiate(
+                        BackImagePrefab[1],
+                        InstansPos,
+                        Quaternion.identity);
+                }
+                break;
+            case 2: //ゴール生成
+                if (BackImagePrefab.Length <= ImageNum)
+                {
+                    Debug.LogError("ゴールプレハブが登録されてないよ");
+                    break;
+                }
+                if (image[0].transform.position.x <= -60)
+                {
+                    Destroy(image[0]);
+                    image[0] = Instantiate(
+                      BackImagePrefab[2],
+                      InstansPos,
+                      Quaternion.identity);
+                }
+                if (image[1].transform.position.x <= -60)
+                {
+                    Destroy(image[1]);
+                    image[1] = Instantiate(
+                      BackImagePrefab[2],
+                      InstansPos,
+                      Quaternion.identity);
+                }
                 break;
         }
         stageCount++;
-        //if(!image[1]==null)
-            
     }
     /// <summary>
     /// 背景を動かす
@@ -95,8 +149,29 @@ public class TrainMove_sanoki : MonoBehaviour {
     {
         if (!moveFlg)//moveFlgがfalseなら中断
             yield break;
-        image[0].transform.position -= new Vector3(Time.deltaTime * scrollSpeed, image[0].transform.position.y, 0);
+        image[0].transform.position -= new Vector3(Time.deltaTime * scrollSpeed, image[0].transform.position.y, 0);//ひたすら左に進む
         image[1].transform.position -= new Vector3(Time.deltaTime * scrollSpeed, image[1].transform.position.y, 0);
         yield return null;
+    }
+
+    /// <summary>
+    /// 時間経過をカウントする
+    /// </summary>
+    /// <param name="seconds">指定した秒数カウントする</param>
+    /// <returns></returns>
+    public IEnumerator Timer(float seconds)
+    {
+        if (isCountDown) yield break;
+        isCountDown = true;
+        timer = 0;
+        float time = 0;
+
+        while (time < 1.0f)
+        {
+            time += Time.deltaTime / seconds;
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        isCountDown = false;
     }
 }
