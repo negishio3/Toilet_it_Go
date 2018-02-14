120d40e1282e081cf08c55d6b837c965be7728d2
Nisshii <nkc.daiki.nishiwaki@gmail.com>
2018-02-14 15:00:53 +0900

    点数計算の作り直し　ランキングの一時中止

---
+++ Assets/master_Scripts/ScoreNoRank_nishiwaki.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreNoRank_nishiwaki : MonoBehaviour {
+
+    /*
+    ScriptはCanvasに入れる
+
+    評価（仮）
+    10点以上　S
+    8点以上　A
+    5点以上　B
+    以下　C
+    */
+
+    // 変更したいスコアテキストを入れる
+    public Text ScoreTime;
+
+    // こなしたギミックの数
+    public int gimmick;
+
+    // かかった時間
+    public float limitTime;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // スコアポイントを評価に変える
+    public void Hyouka(int score)
+    {
+        if (score < 0)
+        {
+            Debug.Log("これはエラーよ！！おかしいわ！！");
+        }
+        else if (score < 5)
+        {
+            Debug.Log("C");
+        }
+        else if (score < 8)
+        {
+            Debug.Log("B");
+        }
+        else if (score < 10)
+        {
+            Debug.Log("A");
+        }
+        else
+        {
+            Debug.Log("S");
+        }
+    }
+
+    public void scorePoint()
+    {
+
+    }
+}
--- Assets/master_Scripts/TouchEffect_mase.cs
+++ Assets/master_Scripts/TouchEffect_mase.cs
@@ -10,27 +10,20 @@ public class TouchEffect_mase : MonoBehaviour
     Camera _camera;//再生させるカメラを入れる
 
     // Use this for initialization
-    void Start()
+    void Start ()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
     {
-
-        if (Input.GetMouseButtonDown(0) || Input.touchCount > 0)//マウスクリック・タップで反応
+        if (Input.GetMouseButtonDown(0) || Input.touchCount>0)//マウスクリック・タップで反応
         {
             // マウス・タップのワールド座標までパーティクルを移動し、パーティクルエフェクトを1つ生成する
-
-        if (Input.GetMouseButtonDown(0) || Input.touchCount > 0)
-            {
-            // マウスのワールド座標までパーティクルを移動し、パーティクルエフェクトを1つ生成する
-
             var pos = _camera.ScreenToWorldPoint(Input.mousePosition + _camera.transform.forward * 10);
-                toucheffect.transform.position = pos;
-                toucheffect.Emit(1);
-            }
+            toucheffect.transform.position = pos;
+            toucheffect.Emit(1);
         }
-    }
+	}
 }
