07f6f56f3ceac4c78b6b6aa15b902e85ec7f4209
HaruRabbi <nkc.harutaka.chikazawa@gmail.com>
2018-02-19 18:15:59 +0900

    ギミックシーン(試作用)/スプリクト(未完成)を追加

---
+++ Assets/master_Scripts/Gimmick_chikazawa.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gimmick_chikazawa : MonoBehaviour {
+
+    public TrainMove_sanoki TM;
+	// Use this for initialization
+	void Start () {
+        TM = FindObjectOfType<TrainMove_sanoki>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(TM.UNKOman==true || col.gameObject.name == "GimmickDestroyer")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
