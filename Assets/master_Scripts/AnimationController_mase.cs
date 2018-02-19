3ae377fe6ed1456bb35cb401c1af9bd3a0056b35
yuya0915 <nkc.yuya.mase.2016@gmail.com>
2018-02-19 14:22:08 +0900

    Revert "何かおかしい"
    This reverts commit 42b309db6d7ec2bb170f239d9b28a1a27d8ce4b3.

--- Assets/master_Scripts/AnimationController_mase.cs
+++ Assets/master_Scripts/AnimationController_mase.cs
@@ -1,32 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class AnimationController_mase : MonoBehaviour
-{
+public class AnimationController_mase : MonoBehaviour {
 
-    Animator animator;
-
-    // Use this for initialization
-    void Start ()
-    {
-        animator = GetComponent<Animator>();
+	// Use this for initialization
+	void Start () {
+		
 	}
 	
 	// Update is called once per frame
-	void Update ()
-    {
+	void Update () {
 		
 	}
-
-    public void wait()
-    {
-        animator.SetTrigger("stop");
-    }
-
-    public void Punch_action()
-    {
-
-    }
 }
