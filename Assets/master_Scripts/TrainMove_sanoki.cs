5a9bc7a66e850efed1638f343a6e37f58f00cd93
NaRu <nkc.hirokazu.sanoki@gmail.com>
2018-02-19 20:13:06 +0900

    背景の追加

--- Assets/master_Scripts/TrainMove_sanoki.cs
+++ Assets/master_Scripts/TrainMove_sanoki.cs
@@ -5,19 +5,37 @@ using UnityEngine.UI;
 
 public class TrainMove_sanoki : MonoBehaviour {
 
-    public TimeCount_murata TimeCount_m;
+    public TimeCount_murata TimeCount_m;//村田パイセンのスクリプトを取得
     public GameObject UNKOman;//キャラクター
     float UNKOman_Speed = 4;  //キャラクターの移動速度
+    
+    //スクロールの速度
+    public float scrollSpeed;//ステージ
+    float BackImage_scrollSpeed = 4;//背景
+    float Pole_scrollSpeed = 16;//電柱
 
-    public float scrollSpeed;//スクロールの速度
     bool moveFlg;//背景を止めるか否か
 
-    public GameObject[] BackImagePrefab;//ステージのプレハブ
+    public GameObject[] TrainPrefab;//ステージのプレハブ
+    public GameObject BackImagePrefab;//背景のプレハブ
+    public GameObject PolePrefab;//電柱のプレハブ
     new SpriteRenderer renderer; //ゲームシーン上でのサイズを取得する用
-    float sizeX;//サイズを保存する
 
-    Vector3 InstansPos;//初期生成位置
-    GameObject[] image = new GameObject[2];//生成したステージを管理する
+    //画像サイズを保存する
+    float TrainSizeX;//電車のサイズを保存する
+    float BackImageSizeX;//背景のサイズを保存する
+    float PoleSizeX;//電柱のサイズを保存する
+    
+    //初期生成位置
+    Vector3 TrainInstansPos;//ステージ
+    Vector3 BackImageInstansPos;//背景
+    Vector3 PoleInstansPos;//電柱
+    
+    //生成したステージを管理する
+    GameObject[] TrainImage = new GameObject[2];
+    GameObject[] BackImage = new GameObject[2];
+    GameObject[] Pole = new GameObject[2];
+
     public int stageCount = 0;//生成された背景のカウンター
 
     int ImageSelector;
@@ -27,21 +45,48 @@ public class TrainMove_sanoki : MonoBehaviour {
 
     public float timer;
     bool isCountDown;
+    public bool buttonFlg = true;
+    public bool isGame;
 
+    
+    /// <summary>
+    /// ステージプレファブ用
+    /// </summary>
     enum TrainState
     {
-        FirstInstans,
-        Loop,
-        Goal
+        FirstInstans,//初期生成
+        Loop,//ループ生成
+        Goal//ゴール生成
     }
 
-	void Start () {
-        renderer = BackImagePrefab[0].GetComponent<SpriteRenderer>();
-        sizeX = renderer.bounds.size.x;
-        InstansPos = new Vector2(sizeX, 0);//生成位置
-        isCountDown = false;
-        moveFlg = false;
+    /// <summary>
+    /// 背景用
+    /// </summary>
+    enum BackImageState
+    {
+        FirstInstans,//初期生成
+        Loop//ループ生成
+    }
+
+    void Start() {
+        //サイズの取得---------------------------------------------------------------------------------------
+        renderer = TrainPrefab[0].GetComponent<SpriteRenderer>();//BackImagePrefab[0]のSpriteRendererを取得
+        TrainSizeX = renderer.bounds.size.x;//生成したときの画像サイズを取得
+        renderer = BackImagePrefab.GetComponent<SpriteRenderer>();
+        BackImageSizeX = renderer.bounds.size.x;//生成したときの画像サイズを取得
+        renderer = PolePrefab.GetComponent<SpriteRenderer>();
+        PoleSizeX = renderer.bounds.size.x;//生成したときの画像サイズを取得
+
+        //生成位置の登録-------------------------------------------------------------------------------------
+        TrainInstansPos = new Vector2(TrainSizeX, 0);//生成位置の更新
+        BackImageInstansPos = new Vector2(BackImageSizeX, 0);
+        PoleInstansPos = new Vector2(PoleSizeX, 0);
+
+        //初期生成-------------------------------------------------------------------------------------------
         TrainImageInstans(TrainState.FirstInstans);//初期背景の生成
+        BackImageInstans(BackImageState.FirstInstans);
+        PoleInstans(BackImageState.FirstInstans);
+
     }
 	
 	void Update () {
@@ -52,10 +97,9 @@ public class TrainMove_sanoki : MonoBehaviour {
             GameStart();
         }
             
-        if (image[0].transform.position.x <= -sizeX || image[1].transform.position.x <= -sizeX)
+        if (TrainImage[0].transform.position.x <= -TrainSizeX || TrainImage[1].transform.position.x <= -TrainSizeX )
         {
-            if (stageCount == maxScrollNum)
-            //if (timer >= maxScrollTime)
+            if (stageCount == maxScrollNum-1)
             {
                 TrainImageInstans(TrainState.Goal);
             }
@@ -64,17 +108,28 @@ public class TrainMove_sanoki : MonoBehaviour {
                 TrainImageInstans(TrainState.Loop);
             }
         }
-        //if (timer >= maxScrollTime && image[1].transform.position.x <= 0)
-        //            moveFlg = false;
-        if (Input.GetMouseButton(0))
+        if(BackImage[0].transform.position.x <= -BackImageSizeX || BackImage[1].transform.position.x <= -BackImageSizeX)
         {
-            //scrollSpeed = 0;
+            BackImageInstans(BackImageState.Loop);
+        }
+        if (Pole[0].transform.position.x <= -PoleSizeX || Pole[1].transform.position.x <= -PoleSizeX)
+        {
+            PoleInstans(BackImageState.Loop);
+        }
+        if (stageCount >= maxScrollNum && (TrainImage[1].transform.position.x <= 0 || TrainImage[0].transform.position.x <= 0))
+        {
+            Debug.Log("よべた");
             moveFlg = false;
         }
-        else if(Input.GetMouseButtonUp(0))
+        if (isGame && Input.GetMouseButton(0))
         {
-            //moveFlg = true;
-            MoveChange();
+            moveFlg = false;
+            scrollSpeed = 0;
+            //moveFlg = false;
+        }
+        else if( isGame && Input.GetMouseButtonUp(0))
+        {
+            moveFlg = true;
             scrollSpeed = 6;
             StartCoroutine(Timer(maxScrollTime));
         }
@@ -88,7 +143,10 @@ public class TrainMove_sanoki : MonoBehaviour {
     public void GameStart()
     {
         moveFlg = true;
+        isGame = true;
         scrollSpeed = 6;
+        BackImage_scrollSpeed = 2;
+        Pole_scrollSpeed = 11;
         StartCoroutine(Timer(maxScrollTime));
     }
 
@@ -98,73 +156,163 @@ public class TrainMove_sanoki : MonoBehaviour {
     /// <param name="ImageNum">番号によって生成方法を変える(整数)</param>
     void TrainImageInstans(TrainState State)
     {
-        moveFlg = false;
         System.Random r = new System.Random();//乱数ジェネレータ
-        ImageSelector = r.Next(BackImagePrefab.Length-1);//最後のプレハブがゴールプレハブに当たるので最大値-１する
+        ImageSelector = r.Next(TrainPrefab.Length-1);//最後のプレハブがゴールプレハブに当たるので最大値-１する
         switch (State)
         {
             case TrainState.FirstInstans: //初期生成
-                image[0] = Instantiate(
-                    BackImagePrefab[ImageSelector],
+                TrainImage[0] = Instantiate(   //image[0]に生成したオブジェクトを登録
+                    TrainPrefab[ImageSelector],
                     Vector2.zero,
                     Quaternion.identity);
                 stageCount++;
-                ImageSelector = r.Next(BackImagePrefab.Length - 1);
-                image[1] = Instantiate(
-                        BackImagePrefab[ImageSelector],
-                        InstansPos,
+                ImageSelector = r.Next(TrainPrefab.Length - 1);
+                TrainImage[1] = Instantiate(
+                        TrainPrefab[ImageSelector],
+                        TrainInstansPos,
                         Quaternion.identity);
                 break;
             case TrainState.Loop: //ループ生成
-                if (image[0].transform.position.x <= -sizeX)//画面外に出たら削除して生成し直す処理
+                if (TrainImage[0].transform.position.x <= -TrainSizeX)//画面外に出たら削除して生成し直す処理
                 {
-                    Destroy(image[0]);
-                    image[0] = Instantiate(
-                        BackImagePrefab[ImageSelector],
-                        InstansPos,
+                    Destroy(TrainImage[0]);
+                    TrainImage[0] = Instantiate(
+                        TrainPrefab[ImageSelector],
+                        TrainInstansPos,
                         Quaternion.identity);
+                    TrainImage[0].transform.position = new Vector2(TrainImage[1].transform.position.x + TrainSizeX,0);//隙間の調整
                 }
-                if (image[1].transform.position.x <= -sizeX)
+                if (TrainImage[1].transform.position.x <= -TrainSizeX)
                 {
-                    Destroy(image[1]);
-                    image[1] = Instantiate(
-                        BackImagePrefab[ImageSelector],
-                        InstansPos,
+                    Destroy(TrainImage[1]);
+                    TrainImage[1] = Instantiate(
+                        TrainPrefab[ImageSelector],
+                        TrainInstansPos,
                         Quaternion.identity);
+                    TrainImage[1].transform.position = new Vector2(TrainImage[0].transform.position.x + TrainSizeX, 0);
                 }
                 break;
             case TrainState.Goal: //ゴール生成
-                if (image[0].transform.position.x <= -sizeX)
+                if (TrainImage[0].transform.position.x <= -TrainSizeX)
                 {
-                    Destroy(image[0]);
-                    image[0] = Instantiate(
-                      BackImagePrefab[BackImagePrefab.Length-1],
-                      InstansPos,
+                    Destroy(TrainImage[0]);
+                    TrainImage[0] = Instantiate(
+                      TrainPrefab[TrainPrefab.Length-1],
+                      TrainInstansPos,
                       Quaternion.identity);
+                    TrainImage[0].transform.position = new Vector2(TrainImage[1].transform.position.x + TrainSizeX, 0);
                 }
-                if (image[1].transform.position.x <= -sizeX)
+                if (TrainImage[1].transform.position.x <= -TrainSizeX)
                 {
-                    Destroy(image[1]);
-                    image[1] = Instantiate(
-                      BackImagePrefab[BackImagePrefab.Length-1],
-                      InstansPos,
+                    Destroy(TrainImage[1]);
+                    TrainImage[1] = Instantiate(
+                      TrainPrefab[TrainPrefab.Length-1],
+                      TrainInstansPos,
                       Quaternion.identity);
+                    TrainImage[1].transform.position = new Vector2(TrainImage[0].transform.position.x + TrainSizeX, 0);
                 }
                 break;
         }
         stageCount++;
-        moveFlg = true;
     }
+
+    /// <summary>
+    /// 背景画像の生成用
+    /// </summary>
+    /// <param name="state">生成方法</param>
+    void BackImageInstans(BackImageState state)
+    {
+        switch (state)
+        {
+            case BackImageState.FirstInstans:
+                BackImage[0] = Instantiate(   //BackImage[0]に生成したオブジェクトを登録
+                  BackImagePrefab,
+                  Vector2.zero,
+                  Quaternion.identity);
+                BackImage[1] = Instantiate(
+                        BackImagePrefab,
+                        BackImageInstansPos,
+                        Quaternion.identity);
+                break;
+            case BackImageState.Loop:
+                if (BackImage[0].transform.position.x <= -BackImageSizeX)//画面外に出たら削除して生成し直す処理
+                {
+                    Destroy(BackImage[0]);
+                    BackImage[0] = Instantiate(
+                        BackImagePrefab,
+                        BackImageInstansPos,
+                        Quaternion.identity);
+                    BackImage[0].transform.position = new Vector2(BackImage[1].transform.position.x + BackImageSizeX, 0);//隙間の調整
+                }
+                if (BackImage[1].transform.position.x <= -BackImageSizeX)
+                {
+                    Destroy(BackImage[1]);
+                    BackImage[1] = Instantiate(
+                        BackImagePrefab,
+                        BackImageInstansPos,
+                        Quaternion.identity);
+                    BackImage[1].transform.position = new Vector2(BackImage[0].transform.position.x + BackImageSizeX, 0);
+                }
+                break;
+        }
+    }
+    /// <summary>
+    /// 電信柱の生成用
+    /// </summary>
+    /// <param name="state">生成方法</param>
+    void PoleInstans(BackImageState state)
+    {
+        switch (state)
+        {
+            case BackImageState.FirstInstans:
+                Pole[0] = Instantiate(   //BackImage[0]に生成したオブジェクトを登録
+                  PolePrefab,
+                  Vector2.zero,
+                  Quaternion.identity);
+                Pole[1] = Instantiate(
+                        PolePrefab,
+                        PoleInstansPos,
+                        Quaternion.identity);
+                break;
+            case BackImageState.Loop:
+                if (Pole[0].transform.position.x <= -PoleSizeX)//画面外に出たら削除して生成し直す処理
+                {
+                    Destroy(Pole[0]);
+                    Pole[0] = Instantiate(
+                        PolePrefab,
+                        PoleInstansPos,
+                        Quaternion.identity);
+                    Pole[0].transform.position = new Vector2(Pole[1].transform.position.x + PoleSizeX, 0);//隙間の調整
+                }
+                if (Pole[1].transform.position.x <= -PoleSizeX)
+                {
+                    Destroy(Pole[1]);
+                    Pole[1] = Instantiate(
+                        PolePrefab,
+                        PoleInstansPos,
+                        Quaternion.identity);
+                    Pole[1].transform.position = new Vector2(Pole[0].transform.position.x + PoleSizeX, 0);
+                }
+                break;
+        }
+    }
+
     /// <summary>
     /// 背景を動かす
     /// </summary>
     /// <returns></returns>
     public IEnumerator TrainMoving()
     {
-        if (!moveFlg)//moveFlgがfalseなら中断
-            yield break;
-        image[0].transform.position -= new Vector3(Time.deltaTime * scrollSpeed, image[0].transform.position.y, 0);//ひたすら左に進む
-        image[1].transform.position -= new Vector3(Time.deltaTime * scrollSpeed, image[1].transform.position.y, 0);
+        if (moveFlg || buttonFlg) //moveFlgかbuttonFlgがfalseなら中断
+        {
+            TrainImage[0].transform.position -= new Vector3(Time.deltaTime * scrollSpeed, TrainImage[0].transform.position.y, 0);//ひたすら左に進む
+            TrainImage[1].transform.position -= new Vector3(Time.deltaTime * scrollSpeed, TrainImage[1].transform.position.y, 0);
+        }
+        BackImage[0].transform.position -= new Vector3(Time.deltaTime * BackImage_scrollSpeed, BackImage[0].transform.position.y, 0);
+        BackImage[1].transform.position -= new Vector3(Time.deltaTime * BackImage_scrollSpeed, BackImage[1].transform.position.y, 0);
+
+        Pole[0].transform.position -= new Vector3(Time.deltaTime * Pole_scrollSpeed, Pole[0].transform.position.y, 0);
+        Pole[1].transform.position -= new Vector3(Time.deltaTime * Pole_scrollSpeed, Pole[1].transform.position.y, 0);
         yield return null;
     }
 
@@ -188,12 +336,16 @@ public class TrainMove_sanoki : MonoBehaviour {
         }
         isCountDown = false;
     }
+    public void moveving()
+    {
+        scrollSpeed = 6;
+    }
 
     /// <summary>
-    /// 外部からmoveFlgをtrueに変更する
+    /// ボタン用のmoveFlgを切り替える
     /// </summary>
-    public void MoveChange()
+   public void ButtonFlgChange()
     {
-        moveFlg = true;
+        buttonFlg = !buttonFlg;
     }
 }
