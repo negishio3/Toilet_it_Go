42b309db6d7ec2bb170f239d9b28a1a27d8ce4b3
yuya0915 <nkc.yuya.mase.2016@gmail.com>
2018-02-19 13:41:52 +0900

    何かおかしい

--- Assets/master_Scripts/AnimationController_mase.cs
+++ Assets/master_Scripts/AnimationController_mase.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class AnimationController_mase : MonoBehaviour {
+public class AnimationController_mase : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
+    Animator animator;
+
+    // Use this for initialization
+    void Start ()
+    {
+        animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    public void wait()
+    {
+        animator.SetTrigger("stop");
+    }
+
+    public void Punch_action()
+    {
+
+    }
 }
