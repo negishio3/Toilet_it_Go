90352732ed21a88e61624a22d434b2ccebb5af48
yuya0915 <nkc.yuya.mase.2016@gmail.com>
2018-02-22 14:56:18 +0900

    新アニメーション作成中

---
+++ Assets/master_Scripts/TouchAnimationController_mase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchAnimationController_mase : MonoBehaviour
+{
+
+   public Animator animator;
+   public float speed;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+        animator.speed = speed;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.touchCount > 0)
+        {
+            animator.SetBool("walk", true);
+            //animator.SetTrigger("walk 0");
+            Debug.Log("うごくちゃんだうー");
+        }
+        else if (Input.GetMouseButtonUp(0) || Input.touchCount < 0)
+        {
+            animator.SetBool("walk", false);
+            Debug.Log("とまるちゃんだお");
+        }
+    }
+}
