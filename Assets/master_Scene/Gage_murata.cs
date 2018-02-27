3f8e299bcbe8322cd935aaf5092a30c2140bc8e5
daimurata <nkc.dai.murata@gmail.com>
2018-02-27 12:13:54 +0900

    長押しとタップを分けた。

---
+++ Assets/master_Scene/Gage_murata.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gage_murata : MonoBehaviour {
+
+    public const int unkoHP = 100;
+
+    public int min_Gage=10;
+
+    public int Center_Gage = 20;
+
+    public int Max_Gage=0;
+
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+}
