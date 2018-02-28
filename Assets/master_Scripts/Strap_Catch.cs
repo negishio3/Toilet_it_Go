4c1d652c2829653849967639ba686ffbf298c7cd
NaRu <nkc.hirokazu.sanoki@gmail.com>
2018-02-28 15:01:29 +0900

    うんこゲージがうまくいかん

---
+++ Assets/master_Scripts/Strap_Catch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Strap_Catch : MonoBehaviour
+{
+
+    Vector2 PlayerPos;
+
+    List<Vector2> StrapPos = new List<Vector2>();
+
+    void Start()
+    {
+        PlayerPos = transform.position;
+    }
+
+    public void StrapCatch()
+    {
+        StrapPos.Clear();
+        
+    }
+}
