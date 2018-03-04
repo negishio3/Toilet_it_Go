c5a9feaa65645dc1c0ab1febff1b29905b5e5be4
yuya0915 <nkc.yuya.mase.2016@gmail.com>
2018-03-04 20:12:37 +0900

    アニメーションの処理追加中

---
+++ Assets/master_Scripts/DoorMove_mase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMove_mase : MonoBehaviour
+{
+    Animator animator;
+    public GameObject Player;
+    public GameObject Goal;
+
+    // Use this for initialization
+    void Start ()
+    {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 Ppos = Player.transform.position;
+        Vector2 Gpos = Goal.transform.position;
+        float dis = Vector2.Distance(Ppos, Gpos);
+        Debug.Log("Distance : " + dis);
+	}
+}
