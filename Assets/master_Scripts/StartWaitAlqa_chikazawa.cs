71f4e8fe36b29b3a9055c9bba786b8d9ff8e2737
HaruRabbi <nkc.harutaka.chikazawa@gmail.com>
2018-03-05 09:34:43 +0900

    土日作業分：明転後に待機を表示するように＆スタート待機イメージを点滅するようにした

---
+++ Assets/master_Scripts/StartWaitAlqa_chikazawa.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartWaitAlqa_chikazawa : MonoBehaviour {
+
+    float alqa = 0;
+    float FadeSpd = 3;
+    public bool stop = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        GetComponent<Image>().color = new Color(0, 0, 0, alqa / 255.0f);
+        if (!stop)
+        {
+            alqa += FadeSpd;// * Time.deltaTime;
+        }
+        if (alqa >= 100.0f)
+        {
+            alqa = 100.0f;
+            stop = true;
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            alqa = 100.0f;
+        }
+    }
+}
