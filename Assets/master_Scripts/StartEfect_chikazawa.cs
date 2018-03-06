5f6a6a338b1f256fe128e7fee50615a7452bf678
HaruRabbi <nkc.harutaka.chikazawa@gmail.com>
2018-03-06 11:01:52 +0900

    作り中

---
+++ Assets/master_Scripts/StartEfect_chikazawa.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartEfect_chikazawa : MonoBehaviour {
+
+    public GameObject start_button;
+
+
+	// Use this for initialization
+	void Start () {
+
+        //start_button = GetComponent<GameObject>().;
+        start_button.SetActive(false);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void BUTTON_ON()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            start_button.SetActive(true);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            start_button.SetActive(false);
+        }
+    }
+}
