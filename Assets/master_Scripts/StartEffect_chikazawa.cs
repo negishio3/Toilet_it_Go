b873e4fbe211a55bb0706be3c4c8b0882635c1a3
HaruRabbi <nkc.harutaka.chikazawa@gmail.com>
2018-03-07 19:41:12 +0900

    ボタンのプレファブ追加   ＊注意＊punk_walk01 haikei_test01に見覚え無し

--- Assets/master_Scripts/StartEffect_chikazawa.cs
+++ Assets/master_Scripts/StartEffect_chikazawa.cs
@@ -5,34 +5,50 @@ using UnityEngine.UI;
 
 public class StartEffect_chikazawa : MonoBehaviour {
 
-    public Image start_button;
-    bool flg = false;
-    float spd = 0.05f;
-    float alqa = 1;
+    public Image start_button;              //拡大する画像
+    public Image[] Clone = new Image[2];    //左右に移動する画僧
+    bool flg = false;                       //ボタンを押した時のフラグ
+    float spd = 0.05f;                      //拡大速度
+    float alqa = 1;                         //α値
 
     // Use this for initialization
     void Start () {
         start_button.GetComponent<RectTransform>();
+        //最初は非表示
+        Clone[0].gameObject.SetActive(false);
+        Clone[1].gameObject.SetActive(false);
         start_button.gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0))//押したら表示。放したら拡散
         {
             flg = true;
-            //start_button.gameObject.SetActive(false);
         }
         if (flg)
         {
-            start_button.rectTransform.localScale += new Vector3(spd,spd);
+            start_button.rectTransform.localScale += new Vector3(spd,spd);     //拡大
+            Clone[0].rectTransform.localPosition += new Vector3(25.0f, 0.0f);  //右に
+            Clone[1].rectTransform.localPosition -= new Vector3(25.0f, 0.0f);  //左に
+            
+            //消える    
             start_button.color = new Color(0, 0, 0, alqa);
+            Clone[0].color = new Color(0, 0, 0, alqa);
+            Clone[1].color = new Color(0, 0, 0, alqa);
+
             alqa -= spd;
         }
     }
 
-    public void BUTTON_DOWN()
+    public void BUTTON_DOWN()//イベントトリガーで設定する
     {
         start_button.gameObject.SetActive(true);
+
+        for (int i = 0; i < 2; i++)
+        {
+            Clone[i].gameObject.SetActive(true);
+        }
+
     }
 }
