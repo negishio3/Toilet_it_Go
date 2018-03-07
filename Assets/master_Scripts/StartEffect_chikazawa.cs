04375877d3be8753abd0fd5a4c08c8618d02836a
HaruRabbi <nkc.harutaka.chikazawa@gmail.com>
2018-03-07 16:54:35 +0900

    ボタンを押した時のエフェクト追加

--- Assets/master_Scripts/StartEffect_chikazawa.cs
+++ Assets/master_Scripts/StartEffect_chikazawa.cs
@@ -5,26 +5,34 @@ using UnityEngine.UI;
 
 public class StartEffect_chikazawa : MonoBehaviour {
 
-    public GameObject start_button;
+    public Image start_button;
+    bool flg = false;
+    float spd = 0.05f;
+    float alqa = 1;
 
-
-	// Use this for initialization
-	void Start () {
-
-        start_button.SetActive(false);
+    // Use this for initialization
+    void Start () {
+        start_button.GetComponent<RectTransform>();
+        start_button.gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetMouseButtonUp(0))
         {
-            start_button.SetActive(false);
+            flg = true;
+            //start_button.gameObject.SetActive(false);
         }
-
+        if (flg)
+        {
+            start_button.rectTransform.localScale += new Vector3(spd,spd);
+            start_button.color = new Color(0, 0, 0, alqa);
+            alqa -= spd;
+        }
     }
 
     public void BUTTON_DOWN()
     {
-        start_button.SetActive(true);
+        start_button.gameObject.SetActive(true);
     }
 }
