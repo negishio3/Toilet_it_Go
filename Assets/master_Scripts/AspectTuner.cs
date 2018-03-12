e1f6ad949541cfc5b1466021acc52ccae61a5ecc
daimurata <nkc.dai.murata@gmail.com>
2018-03-12 19:26:46 +0900

    Imageのサイズを自動調整するスクリプトの追加

---
+++ Assets/master_Scripts/AspectTuner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AspectTuner : MonoBehaviour {
+
+    public RectTransform CanvasSize;
+    public Image ThisImage;
+
+	// Use this for initialization
+	void Start () {
+		ThisImage.rectTransform.sizeDelta= new Vector2((ThisImage.sprite.bounds.size.x * CanvasSize.sizeDelta.y) / ThisImage.sprite.bounds.size.y, CanvasSize.sizeDelta.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
